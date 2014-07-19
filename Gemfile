3be54e186714629b1cf50d04952e1d023d5f0499
pcrglennon <pcrglennon@gmail.com>
2014-07-19 16:32:11 -0400

    Create .gitignore, set environment to load config options from a config.yml file

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@ gem 'sqlite3'
 gem 'sinatra'
 gem 'thin'
 gem 'rake'
-gem 'figaro'
 
 group :test, :development do
   gem 'pry'
--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,13 @@
 require 'bundler'
-Bundle.require(:default)
+Bundler.require(:default)
 
 DB = SQLite3::Database.open "locations.db"
+
+configure do
+  yaml = YAML.load_file(settings.config + "/config.yaml")[settings.environment.to_s]
+  yaml.each_pair do |key, value|
+    set(key.to_sym, value)
+  end
+end
 
 require 'app'
