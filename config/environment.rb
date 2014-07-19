3e77c666f938f765b75f14fe17fba4a09d466eab
pcrglennon <pcrglennon@gmail.com>
2014-07-19 17:24:14 -0400

    Fix environment file to default to 'development' env, add db to .gitignore

--- config/environment.rb
+++ config/environment.rb
@@ -1,13 +1,16 @@
+ENV['SINATRA_ENV'] ||= "development"
+
 require 'bundler'
-Bundler.require(:default)
+Bundler.require(:default, ENV['SINATRA_ENV'])
 
 DB = SQLite3::Database.open "locations.db"
 
+require 'yaml'
 configure do
-  yaml = YAML.load_file(settings.config + "/config.yaml")[settings.environment.to_s]
+  yaml = YAML.load_file("config/config.yml")[settings.environment.to_s]
   yaml.each_pair do |key, value|
     set(key.to_sym, value)
   end
 end
 
-require 'app'
+# require 'app'
