3abefe0332c784ad667068dc20806a94974933b7
pcrglennon <pcrglennon@gmail.com>
2014-07-20 17:05:18 -0400

    POST '/' route

--- app.rb
+++ app.rb
@@ -4,4 +4,15 @@ class App < Sinatra::Base
   get '/' do
     erb :'index'
   end
+
+  get '/:map_name' do
+
+  end
+
+  post '/' do
+    location = Location.new(params[:location])
+    # TODO - validate that SQL was executed correctly!
+    location.save
+    redirect to("/#{location.map_name}")
+  end
 end
