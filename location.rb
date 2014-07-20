03d5f760b629a3b06b90d8931339e0f05ede80d3
pcrglennon <pcrglennon@gmail.com>
2014-07-20 17:04:47 -0400

    Start of Mini-ORM for Location: create, update, and normalize params

--- location.rb
+++ location.rb
@@ -1,4 +1,7 @@
+require 'uri'
+
 class Location
+  # Class Methods
 
   def self.create_table
     migrate = <<-SQL
@@ -19,5 +22,53 @@ class Location
     DB[:conn].execute(drop)
   end
 
+  def self.create(params)
+    location = self.new(params)
+    location.save
+    location
+  end
 
+  # Instance Methods
+
+  attr_reader :id, :map_name, :latitude, :longitude
+
+  def initialize(params)
+    normalize_params(params)
+  end
+
+  def save
+    persisted? ? update : insert
+  end
+
+  def persisted?
+    !!self.id
+  end
+
+  def insert
+    insert = <<-SQL
+      INSERT INTO locations (map_name, latitude, longitude)
+      VALUES (?, ?, ?)
+    SQL
+    DB[:conn].execute(insert, map_name, latitude, longitude)
+    @id = DB[:conn].last_insert_row_id
+  end
+
+  def update
+    update = <<-SQL
+      UPDATE locations
+      SET (map_name = ?, latitude = ?, longitude = ?)
+      WHERE id = ?
+    SQL
+    DB[:conn].execute(update, id, map_name, latitude, longitude)
+  end
+
+  private
+
+    def normalize_params(params)
+      # Protect against HTML injection by encoding map_name
+      @map_name = URI.encode(params[:map_name])
+      # to_f would reject any HTML in the string already, so this is enough
+      @latitude = params[:latitude].to_f
+      @longitude = params[:longitude].to_f
+    end
 end
