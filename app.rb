40ef29bec74c68d85f83dfebe12cdfab764db1c6
pcrglennon <pcrglennon@gmail.com>
2014-07-25 13:45:49 -0400

    Move config options (Maps API Key) to App.rb so they are properly accessible

--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
 require './config/environment'
+require 'yaml'
 
 class App < Sinatra::Base
+
+  # config.yml file must be created first (in config folder)
+  configure do
+    yaml = YAML.load_file("config/config.yml")[settings.environment.to_s]
+    yaml.each_pair do |key, value|
+      set(key.to_sym, value)
+    end
+  end
+
   get '/' do
+    binding.pry
     erb :'index'
   end
 
--- config/environment.rb
+++ config/environment.rb
@@ -5,14 +5,5 @@ Bundler.require(:default, ENV['SINATRA_ENV'])
 
 DB = {:conn => SQLite3::Database.new("./db/locations.db")}
 
-require 'yaml'
-
-# config.yml file must be created first (in config folder)
-configure do
-  yaml = YAML.load_file("config/config.yml")[settings.environment.to_s]
-  yaml.each_pair do |key, value|
-    set(key.to_sym, value)
-  end
-end
 
 require './location'
