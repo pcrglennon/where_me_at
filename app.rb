034ad67bcae7f7f923eb512c15db10b154a76d5f
Justin Kim <justinjmkim@gmail.com>
2014-07-27 13:54:28 -0400

    Change ternary operator to if else statement

--- app.rb
+++ app.rb
@@ -38,7 +38,11 @@ class App < Sinatra::Base
 
   get '/:map_name' do
     @location = Location.find_by_map_name(params[:map_name])
-    !!@location ? erb :'show' : redirect to('/error')
+    if @location 
+      erb :'show' 
+    else 
+      redirect to('/error')
+    end
   end
 
 end
