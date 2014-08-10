8fbc965ca663e9bdbe0b12288a472e9813cd751b
pcrglennon <pcrglennon@gmail.com>
2014-08-09 21:26:45 -0400

    Add some initial tests for testing creating a map and sending messages in one POST (NYI)

--- spec/routing_spec.rb
+++ spec/routing_spec.rb
@@ -19,14 +19,14 @@ describe 'Routes' do
   end
 
   describe 'POST /' do
+    before do
+      @location_params = {map_name: "a-test-map", latitude: 42.0, longitude: 42.0}
+    end
+
     context 'creating a map with a unique name' do
       before do
         post '/', {
-          :location => {
-            :map_name => 'a-test-map',
-            :latitude => 42.0,
-            :longitude => 42.0
-          }
+          location: @location_params
         }
         follow_redirect!
       end
@@ -38,9 +38,54 @@ describe 'Routes' do
       it 'redirects to the page for the new map' do
         expect(last_request.url).to end_with('/a-test-map')
       end
+
+      context 'and sending messages' do
+        context 'to a valid phone number' do
+          before do
+            post '/', {
+              location: @location_params,
+              addresses: [5555555555]
+            }
+          end
+
+          it 'responds with a 200 status code' do
+            expect(last_response).to be_ok
+          end
+
+          it 'redirects back to the home page with a success message' do
+            expect(last_request.path).to eq("/")
+            expect(last_response.body).to include('Message(s) sent successfully')
+          end
+        end
+      end
     end
 
     context 'creating a map with a name already in use' do
+      before do
+        Location.create(:map_name => 'a-test-map', :latitude => 42.0, :longitude => 42.0)
+        post '/', {
+          location: @location_params
+        }
+        follow_redirect!
+      end
+
+      it 'responds with a 200 status code' do
+        expect(last_response).to be_ok
+      end
+
+      it 'redirects back to the home page with an error message' do
+        expect(last_request.path).to eq("/error")
+        expect(last_request.query_string).to eq("msg=map_name_in_use")
+        expect(last_response.body).to include('There is already a map with that name.')
+      end
+
+      it 'does not create another map with the same name, and does not override the original' do
+        expect(Location.where(map_name: "a-test-map").size).to eq(1)
+        expect(Location.find_by(map_name: "a-test-map").latitude).to eq(42.0)
+      end
+    end
+
+    context 'creating a map and sending' do
       before do
         Location.create(:map_name => 'a-test-map', :latitude => 42.0, :longitude => 42.0)
         post '/', {
@@ -51,20 +96,6 @@ describe 'Routes' do
           }
         }
         follow_redirect!
-      end
-
-      it 'responds with a 200 status code' do
-        expect(last_response).to be_ok
-      end
-
-      it 'redirects back to the home page with an error message' do
-        expect(last_request.url).to end_with('error?msg=map_name_in_use')
-        expect(last_response.body).to include('There is already a map with that name.')
-      end
-
-      it 'does not create another map with the same name, and does not override the original' do
-        expect(Location.where(map_name: "a-test-map").size).to eq(1)
-        expect(Location.find_by(map_name: "a-test-map").latitude).to eq(42.0)
       end
     end
   end
