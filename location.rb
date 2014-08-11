64e7f2404c0d7ca5118cf206948df83aeb027b93
pcrglennon <pcrglennon@gmail.com>
2014-08-11 13:13:37 -0400

    Rename denormalized_name to denormalized_map_name, some new CSS styling

--- location.rb
+++ location.rb
@@ -2,8 +2,7 @@ class Location < ActiveRecord::Base
   validates_presence_of :map_name, :latitude, :longitude
   validates_uniqueness_of :map_name
 
-  def denormalized_name
-    self.map_name.gsub('-', ' ')
+  def denormalized_map_name
+    map_name.gsub('-', ' ')
   end
-
 end
